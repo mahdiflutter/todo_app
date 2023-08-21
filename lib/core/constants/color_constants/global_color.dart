04e9e6ab4ab4d593726f5e495decea1494512e94
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-21 16:00:58 +0330

    Create

---
+++ lib/core/constants/color_constants/global_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GlobalColor {
+  static const Color background = Color(0xffFAFAFA);
+  static const Color deepGreen = Color(0xff18DAA3);
+  static const Color lightGreen = Color(0xffE2F6F1);
+  static const Color deepBlue = Color(0xff334BF3);
+  static const Color deepOrange = Color(0xffFE951A);
+  static const Color deepRed = Color(0xffFF5252);
+  static const Color textGrey = Color(0xffBFC3C8);
+}
