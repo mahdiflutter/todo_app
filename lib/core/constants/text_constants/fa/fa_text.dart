8e4fe65ea874992c143f882e002d1c6b1bfba8cd
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-21 16:00:52 +0330

    Create

---
+++ lib/core/constants/text_constants/fa/fa_text.dart
@@ -0,0 +1,32 @@
+class FaWord {
+  static const String hello = 'سلام';
+  static const String activeTask = 'تسک های امروز';
+  static const String todayTasks = 'تسک فعال';
+  static const String doneTasks = 'تسک های انجام شده';
+  static const String todayActiveTasks = 'تسک فعال در امروز';
+  static const String category = 'دسته بندی';
+  static const String viewMore = 'مشاهده بیشتر';
+  static const String all = 'همه';
+  static const String reverseTimer = 'شمارنده معکوس';
+  static const String saveTimer = 'شمارنده های ذخیره';
+  static const String doneTimer = 'شمارنده های انجام شده';
+  static const String stop = 'توقف';
+  static const String finish = 'پایان';
+  static const String continuation = 'ادامه';
+  static const String edit = 'ویرایش';
+  static const String subject = 'عنوان';
+  static const String description = 'توضیحات';
+  //categories
+  static const String sport = 'ورزش';
+  static const String meet = 'جلسه';
+  static const String learn = 'یادگیری';
+  static const String buy = 'خرید';
+  static const String another = 'دیگر دسته بندی ها';
+}
+
+class FaText {
+  //sentences
+  static const String successAddTask = 'تسک مورد نظر با موفقیت اضافه شد';
+  static const String successEditTask = 'تسک مورد نظر با موفقیت ویرایش شد';
+  static const String successDeleteTask = 'تسک مورد نظر با موفقیت حذف شد';
+}
