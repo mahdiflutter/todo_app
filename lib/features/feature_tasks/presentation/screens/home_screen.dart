dc2e851e2036339ef7e62b6c0ab608eb4d5c09f0
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-22 17:33:01 +0330

    Add Task Lists

--- lib/features/feature_tasks/presentation/screens/home_screen.dart
+++ lib/features/feature_tasks/presentation/screens/home_screen.dart
@@ -9,47 +9,74 @@ import 'package:task_cut_app/core/widgets/middle_text.dart';
 import 'package:task_cut_app/core/widgets/small_text.dart';
 import 'package:task_cut_app/features/feature_tasks/presentation/widgets/category_cart.dart';
 import 'package:task_cut_app/features/feature_tasks/presentation/widgets/profile_container.dart';
+import 'package:task_cut_app/features/feature_tasks/presentation/widgets/task_cart.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MainContainer(
+    return const MainContainer(
       body: CustomScrollView(
         slivers: <Widget>[
-          const Header(),
-          const SearchBox(),
-          const CategoriesSlider(),
-          SliverPadding(
-            padding: const EdgeInsets.only(top: 32),
-            sliver: SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: const <BoxShadow>[
-                        BoxShadow(
-                          color: GlobalColor.textGrey,
-                          blurRadius: 5,
-                          spreadRadius: 0.5,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                mainAxisExtent: 132,
-                mainAxisSpacing: 20,
-              ),
-            ),
-          )
+          Header(),
+          SearchBox(),
+          CategoriesSlider(),
+          TaskListHeader(),
+          TaskList()
         ],
+      ),
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  const TaskList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return const TaskCart(done: false);
+          },
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+          mainAxisExtent: 132,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
+
+class TaskListHeader extends StatelessWidget {
+  const TaskListHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24,
+        vertical: 20,
+      ),
+      sliver: SliverToBoxAdapter(
+        child: Row(
+          children: <Widget>[
+            MiddleText(
+              text: '${FaWord.todayTasks} ✅',
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -62,26 +89,29 @@ class CategoriesSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Column(
-        children: <Widget>[
-          const Row(
-            children: <Widget>[
-              MiddleText(text: 'دسته بندی'),
-            ],
-          ),
-          const SizedBox(height: 10),
-          SizedBox(
-            height: 183,
-            child: ListView.builder(
-              itemCount: 3,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return const CategoryCart();
-              },
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      sliver: SliverToBoxAdapter(
+        child: Column(
+          children: <Widget>[
+            const Row(
+              children: <Widget>[
+                MiddleText(text: 'دسته بندی'),
+              ],
             ),
-          ),
-        ],
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 183,
+              child: ListView.builder(
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const CategoryCart();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -97,6 +127,7 @@ class SearchBox extends StatelessWidget {
     return SliverPadding(
       padding: const EdgeInsets.symmetric(
         vertical: 32,
+        horizontal: 24,
       ),
       sliver: SliverToBoxAdapter(
           child: Container(
@@ -143,49 +174,55 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          ProfileContainer(
-            child: Image.asset(
-              'assets/images/man.png',
-            ),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-          const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  LargeText(text: FaWord.hello),
-                  LargeText(text: '، '),
-                  LargeText(
-                    text: 'مهدی',
-                    color: GlobalColor.deepGreen,
-                  ),
-                  LargeText(text: '👋'),
-                ],
-              ),
-              SmallText(
-                text: '2 شهریور',
-                color: GlobalColor.textGrey,
-              ),
-            ],
-          ),
-          const Spacer(),
-          const SizedBox(
-            height: 26,
-            child: CustomBadge(
-              child: SmallText(
-                text: '20 ${FaWord.activeTask}',
-                color: GlobalColor.deepGreen,
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+        vertical: 25,
+      ),
+      sliver: SliverToBoxAdapter(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            ProfileContainer(
+              child: Image.asset(
+                'assets/images/man.png',
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              width: 15,
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    LargeText(text: FaWord.hello),
+                    LargeText(text: '، '),
+                    LargeText(
+                      text: 'مهدی',
+                      color: GlobalColor.deepGreen,
+                    ),
+                    LargeText(text: '👋'),
+                  ],
+                ),
+                SmallText(
+                  text: '2 شهریور',
+                  color: GlobalColor.textGrey,
+                ),
+              ],
+            ),
+            const Spacer(),
+            const SizedBox(
+              height: 26,
+              child: CustomBadge(
+                child: SmallText(
+                  text: '20 ${FaWord.activeTask}',
+                  color: GlobalColor.deepGreen,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
